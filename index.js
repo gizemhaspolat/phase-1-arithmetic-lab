0ba50fe7d672cdc9e44f83ec28fa06e17d77cd8c
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-04-19 17:18:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(7, 19, 20);
